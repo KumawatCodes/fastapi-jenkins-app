fa89dcbdba9a60ccef8d61a50f2e7eb8c8215208
Himanshu <himanshu.k19102004@gmail.com>
2026-05-15 00:11:46 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        'message' : "hello from flask in docker && this check of webhook",
+        'message' : "hello from flask in docker hello world",
         'status': 'Live'
     })
 
